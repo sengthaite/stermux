d5ab25145492b45ab8b154565dc175a0877f18ad
sengthaite <sengthaite@gmail.com>
2026-04-24 14:05:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 export REPO_URL="https://sengthaite.github.io/stermux"
 
+echo "Make sources.list.d if not exist"
+mkdir -p $PREFIX/etc/apt/sources.list.d/
+
 curl -L "$REPO_URL/stermux.gpg" | gpg --dearmor | tee /data/data/com.termux/files/usr/etc/apt/trusted.gpg.d/stermux.gpg > /dev/null
 
 echo "deb $REPO_URL termux extras" > $PREFIX/etc/apt/sources.list.d/stermux.list
